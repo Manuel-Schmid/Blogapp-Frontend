63418e6ed14ec08b4b02a47e201e1a04f80bf88a
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-11-29 14:40:42 +0100

    MBP-080: create Component tests for PostTile- & ProfileComponent

---
+++ src/__tests__/ComponentTests/PostTileComponent.spec.js
@@ -0,0 +1,46 @@
+import { describe, it, expect } from "vitest";
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
+import PostTileComponent from "../../components/posts-overview/PostTileComponent.vue";
+
+describe("PostTileComponent", () => {
+  it("should render post data correctly", () => {
+    const wrapper = shallowMount(PostTileComponent, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true,
+          RouterLink: RouterLinkStub,
+        },
+      },
+      propsData: {
+        post: {
+          title: "Fortes Fortuna Adiuvat",
+          slug: "fortes-fortuna-adjuvat",
+          image: {
+            name: "images/Landscape-Color.jpg",
+          },
+          dateCreated: "2022-08-16T06:31:23.250000+00:00",
+          likeCount: "3",
+          commentCount: "2",
+          category: {
+            name: "Comp Sci",
+            slug: "comp-sci",
+          },
+          owner: {
+            firstName: "Arthur C.",
+            lastName: "Clarke",
+          },
+        },
+      },
+    });
+
+    expect(wrapper.findAll("p").length).toEqual(3);
+    expect(wrapper.findAll("p").at(0).text()).toMatch("Fortes Fortuna Adiuvat");
+    expect(wrapper.findAll("p").at(1).text()).toMatch(
+      "Arthur C. Clarke - 16/08/2022"
+    );
+    expect(wrapper.findAll("p").at(2).text()).toMatch("Comp Sci");
+    expect(wrapper.findAll("img").at(0).attributes().src).toMatch(
+      "http://api.blogapp.com/media/images/Landscape-Color.jpg"
+    );
+  });
+});
---
+++ src/__tests__/ComponentTests/ProfileComponent.spec.js
@@ -0,0 +1,95 @@
+import { describe, it, expect } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import ProfileComponent from "../../components/ProfileComponent.vue";
+
+const defaultProps = {
+  userData: {
+    id: "1",
+    username: "admin",
+    email: "admin@example.lo",
+    firstName: "Winston",
+    lastName: "Smith",
+    userStatus: {
+      verified: true,
+      archived: false,
+      secondaryEmail: null,
+      isAuthor: false,
+    },
+  },
+  authorRequest: null,
+  firstNameEditable: false,
+  lastNameEditable: false,
+  emailChangeEmailSent: false,
+};
+
+const mountOptions = {
+  global: {
+    stubs: {
+      FontAwesomeIcon: true,
+    },
+  },
+  propsData: defaultProps,
+};
+
+describe("ProfileComponent.vue Tests", () => {
+  it("should display the user data in a table", () => {
+    const wrapper = shallowMount(ProfileComponent, mountOptions);
+
+    expect(wrapper.findAll("table").length).toEqual(1);
+    expect(wrapper.findAll("div").at(3).text()).contains(
+      "UsernameadminFirst nameWinston Last nameSmith Emailadmin@example.lo"
+    );
+  });
+
+  it("should display that the user has author permissions", () => {
+    const customProps = JSON.parse(JSON.stringify(defaultProps));
+    customProps.userData.userStatus.isAuthor = true;
+    const customMountOptions = JSON.parse(JSON.stringify(mountOptions));
+    customMountOptions.propsData = customProps;
+    const wrapper = shallowMount(ProfileComponent, customMountOptions);
+
+    expect(wrapper.findAll("div").length).toEqual(10);
+    expect(wrapper.findAll("div").at(5).text()).toMatch("You are an author");
+  });
+
+  it("should display an option to request author permissions", () => {
+    const wrapper = shallowMount(ProfileComponent, mountOptions);
+
+    expect(wrapper.findAll("div").length).toEqual(7);
+    expect(wrapper.findAll("div").at(2).text()).toMatch("Become an author");
+  });
+
+  it("should display info about the pending author request including its creation date", () => {
+    const customProps = JSON.parse(JSON.stringify(defaultProps));
+    customProps.authorRequest = {
+      dateOpened: "2022-11-18T09:28:51.116911+00:00",
+      dateClosed: null,
+      status: "PENDING",
+    };
+    const customMountOptions = JSON.parse(JSON.stringify(mountOptions));
+    customMountOptions.propsData = customProps;
+    const wrapper = shallowMount(ProfileComponent, customMountOptions);
+
+    expect(wrapper.findAll("div").length).toEqual(10);
+    expect(wrapper.findAll("div").at(2).text()).toMatch(
+      "Your author request was submitted on 18/11/2022 and is still pending"
+    );
+  });
+
+  it("should inform the user about his rejected author request including the date of the rejection", () => {
+    const customProps = JSON.parse(JSON.stringify(defaultProps));
+    customProps.authorRequest = {
+      dateOpened: "2022-11-18T09:28:51.116911+00:00",
+      dateClosed: "2022-11-20T09:28:51.116911+00:00",
+      status: "REJECTED",
+    };
+    const customMountOptions = JSON.parse(JSON.stringify(mountOptions));
+    customMountOptions.propsData = customProps;
+    const wrapper = shallowMount(ProfileComponent, customMountOptions);
+
+    expect(wrapper.findAll("div").length).toEqual(10);
+    expect(wrapper.findAll("div").at(2).text()).toMatch(
+      "Your author request was rejected on 20/11/2022"
+    );
+  });
+});
