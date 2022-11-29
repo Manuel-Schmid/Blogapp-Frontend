794be11a6b6a201162117a73e94170c58e316120
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-11-29 14:28:38 +0100

    MBP-080: add Unittests for helper functions

---
+++ src/__tests__/UnitTests/helper.spec.js
@@ -0,0 +1,64 @@
+import { describe, it, expect } from "vitest";
+import {
+  formatDateLong,
+  formatDateShort,
+  formatFullname,
+  getImageURL,
+} from "../../helper/helper.ts";
+
+describe("Helper functions", () => {
+  it("should return date in format 'dd/mm/yyyy'", () => {
+    const formattedDateShort = formatDateShort(
+      "2022-11-20T09:28:51.116911+00:00"
+    );
+    expect(formattedDateShort).toMatch("20/11/2022");
+  });
+
+  it("should return an invalid date", () => {
+    const formattedDateShort = formatDateShort(
+      "2022-11-40T09:28:51.116911+00:00"
+    );
+    expect(formattedDateShort).toMatch("Invalid Date");
+  });
+
+  it("should return date in format 'DD, dd/mm/yyyy'", () => {
+    const formattedDateLong = formatDateLong(
+      "2022-11-20T09:28:51.116911+00:00"
+    );
+    expect(formattedDateLong).toMatch("Sunday, 20/11/2022");
+  });
+
+  it("should return date in format 'DD, dd/mm/yyyy'", () => {
+    const formattedDateLong = formatDateLong(
+      "2022-11-23T09:28:51.116911+00:00"
+    );
+    expect(formattedDateLong).toMatch("Wednesday, 23/11/2022");
+  });
+
+  it("should return an invalid date", () => {
+    const formattedDateLong = formatDateLong(
+      "2022-13-20T09:28:51.116911+00:00"
+    );
+    expect(formattedDateLong).toMatch("Invalid Date");
+  });
+
+  it("should return a correctly formatted image url", () => {
+    const imageURL = getImageURL("images/lake.jpg");
+    expect(imageURL).toMatch("http://api.blogapp.com/media/images/lake.jpg");
+  });
+
+  it("should return first and last name separated by a whitespace", () => {
+    const imageURL = formatFullname("Winston", "Smith");
+    expect(imageURL).toMatch("Winston Smith");
+  });
+
+  it("should return a trimmed string of just the first name", () => {
+    const imageURL = formatFullname("George", "");
+    expect(imageURL).toEqual("George");
+  });
+
+  it("should return a trimmed string of just the last name", () => {
+    const imageURL = formatFullname("", "Orwell");
+    expect(imageURL).toEqual("Orwell");
+  });
+});
