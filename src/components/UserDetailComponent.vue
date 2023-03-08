4030513a05e041734edfc7770fe9ca33030bab2d
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2023-03-08 08:20:29 +0100

    MBP-103: add UserDetail Container & Component

---
+++ src/components/UserDetailComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+  name: "UserDetailComponent",
+  props: ["userData"],
+
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="site-container">
+    {{ userData }}
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/container/UserDetailContainer.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import UserDetailComponent from "../components/UserDetailComponent.vue";
+import { usePostStore } from "../store/blog";
+
+export default {
+  name: "UserDetailContainer",
+  components: { UserDetailComponent },
+
+  setup() {
+    const postStore = usePostStore();
+
+    return { postStore };
+  },
+};
+</script>
+
+<template>
+  <UserDetailComponent
+    v-if="postStore.userDetail"
+    :user-data="postStore.userDetail"
+  ></UserDetailComponent>
+</template>
